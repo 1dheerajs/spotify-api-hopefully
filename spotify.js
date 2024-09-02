6a4448fa1e6e9b12a8fecea54759e1f8179a7982
1dheerajs <76550271+1dheerajs@users.noreply.github.com>
2024-09-03 01:34:00 +0530

    spotify api maby

---
+++ spotify.js
@@ -0,0 +1,42 @@
+const APIController = (function() {
+    
+    const clientId = 'PASTE YOUR CLIENT ID HERE';
+    const clientSecret = 'PASTE YOUR CLIENT SECRET HERE';
+
+    // private methods
+    
+    
+
+    const _getTracks = async (token, tracksEndPoint) => {
+
+        const limit = 10;
+
+        const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.items;
+    }
+
+    const _getTrack = async (token, trackEndPoint) => {
+
+        const result = await fetch(`${trackEndPoint}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data;
+    }
+
+    return {
+        getTracks(token, tracksEndPoint) {
+            return _getTracks(token, tracksEndPoint);
+        },
+        getTrack(token, trackEndPoint) {
+            return _getTrack(token, trackEndPoint);
+        }
+    }
+})();
